2c5552c5e8267c085267e5d4deea5d173b211662
Phi <phi@Phis-MacBook-Pro.local>
2023-04-07 14:19:36 +0200

    code updated

--- src/JobsImporter.php
+++ src/JobsImporter.php
@@ -4,11 +4,11 @@ class JobsImporter
 {
     private PDO $db;
 
-    private string $file;
+    private array $files;
 
-    public function __construct(string $host, string $username, string $password, string $databaseName, string $file)
+    public function __construct(string $host, string $username, string $password, string $databaseName, array $files)
     {
-        $this->file = $file;
+        $this->files = $files;
         
         /* connect to DB */
         try {
@@ -23,13 +23,32 @@ class JobsImporter
         /* remove existing items */
         $this->db->exec('DELETE FROM job');
 
-        /* parse XML file */
-        $xml = simplexml_load_file($this->file);
+        $i = 0;
 
+        if (count($this->files) === 0) {
+            throw new Exception("No files to read");
+        }
+
+        foreach ($this->files as $file) {
+            printMessage("Reading file: ". $file['file']);
+            if  ($file['extension'] === 'json') {
+                $i += $this->importJson($file['file']);
+            } elseif ($file['extension'] === 'xml') {
+                $i += $this->importXml($file['file']);
+            }
+        }
+        /* parse XML file */
+        return $i;
+    }
+
+    private function importXml(string $file): int
+    {
+        $xml = simplexml_load_file($file);
+
+        $commands = 0;
         /* import each item */
-        $count = 0;
         foreach ($xml->item as $item) {
-            $this->db->exec('INSERT INTO job (reference, title, description, url, company_name, publication) VALUES ('
+            $commands += $this->db->exec('INSERT INTO job (reference, title, description, url, company_name, publication) VALUES ('
                 . '\'' . addslashes($item->ref) . '\', '
                 . '\'' . addslashes($item->title) . '\', '
                 . '\'' . addslashes($item->description) . '\', '
@@ -37,8 +56,28 @@ class JobsImporter
                 . '\'' . addslashes($item->company) . '\', '
                 . '\'' . addslashes($item->pubDate) . '\')'
             );
-            $count++;
         }
-        return $count;
+        return $commands;
+    }
+
+    private function importJson(string $file): int
+    {
+        $fileContent = json_decode(file_get_contents($file), true);
+        $items = $fileContent["offers"];
+        $prefix = $fileContent["offerUrlPrefix"];
+        $commands = 0;
+
+        foreach ($items as $item) {
+            $date = DateTimeImmutable::createFromFormat('D M d H:i:s e Y', $item['publishedDate']);
+
+            $commands += $this->db->exec('INSERT INTO job (reference, title, description, url, company_name, publication) VALUES ('
+               . '\''. addslashes($item['reference']) . '\', '
+               . '\''. addslashes($item['title']) . '\', '
+               . '\''. addslashes($item['description']) . '\', '
+               . '\''. addslashes($prefix . $item['urlPath']) . '\', '
+               . '\''. addslashes($item['companyname']) . '\', '
+               . '\''. $date->format('Y-m-d') . '\')');
+        }
+        return $commands;
     }
 }
--- src/app.php
+++ src/app.php
@@ -11,20 +11,38 @@ include_once(__DIR__.'/config.php');
 
 printMessage("Starting...");
 
+$files = [];
+$count = 0;
 
-/* import jobs from regionsjob.xml */
-$jobsImporter = new JobsImporter(SQL_HOST, SQL_USER, SQL_PWD, SQL_DB, RESSOURCES_DIR . 'regionsjob.xml');
-$count = $jobsImporter->importJobs();
+if($folder = opendir(RESSOURCES_DIR))
+    {
+        while(($file = readdir($folder)))
+        {
+					$ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+					if (in_array($ext, ['xml', 'json'])) {
+						$files[] = [
+							'extension' => $ext,
+							'file' => RESSOURCES_DIR . $file
+						];
+					}
+        }
+     }
 
-printMessage("> {count} jobs imported.", ['{count}' => $count]);
+		$jobsImporter = new JobsImporter(SQL_HOST, SQL_USER, SQL_PWD, SQL_DB, $files);
 
+		try {
+			$count = $jobsImporter->importJobs();
+		} catch (Exception $e) {
+			printMessage("An error occurred");
+		}
+		printMessage("> {count} jobs imported.", ['{count}' => $count]);
 
 /* list jobs */
-$jobsLister = new JobsLister(SQL_HOST, SQL_USER, SQL_PWD, SQL_DB);
-$jobs = $jobsLister->listJobs();
+		$jobsLister = new JobsLister(SQL_HOST, SQL_USER, SQL_PWD, SQL_DB);
+		$jobs = $jobsLister->listJobs();
 
-printMessage("> all jobs ({count}):", ['{count}' => count($jobs)]);
-foreach ($jobs as $job) {
+		printMessage("> all jobs ({count}):", ['{count}' => count($jobs)]);
+		foreach ($jobs as $job) {
     printMessage(" {id}: {reference} - {title} - {publication}", [
     	'{id}' => $job['id'],
     	'{reference}' => $job['reference'],
